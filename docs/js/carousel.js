9cb797febaaaf4b7a596e9f5d19c6a86e0501623
yk <rlduf1028@gmail.com>
2024-08-24 22:38:22 +0900

    f

--- docs/js/carousel.js
+++ docs/js/carousel.js
@@ -1,3 +1,7 @@
 import { Carousel } from "@fancyapps/ui/dist/carousel/carousel.esm.js";
 import "@fancyapps/ui/dist/carousel/carousel.css";
 
+const container = document.getElementById("myCarousel");
+const options = { infinite: false };
+
+new Carousel(container, options);
